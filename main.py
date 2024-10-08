ab686ea7b190480bc960b03bbc60639941d1e21d
yuHsuan <x89301089@gmail.com>
2024-10-08 10:08:58 +0800

    [modify] chat bot use langgraph

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage
-
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import START, MessagesState, StateGraph
 model = ChatOpenAI(model="gpt-3.5-turbo")
+# Define a new graph
+workflow = StateGraph(state_schema=MessagesState)
+# Define the function that calls the model
+def call_model(state: MessagesState):
+    response = model.invoke(state["messages"])
+    return {"messages": response}
+
+# Define the (single) node in the graph
+workflow.add_edge(START, "model")
+workflow.add_node("model", call_model)
+
+# Add memory
+memory = MemorySaver()
+app = workflow.compile(checkpointer=memory)
+
+config = {"configurable": {"thread_id": "abc123"}}
 
 input_text = input('>>> ')
 while input_text.lower() != 'bye':
     if input_text:
-        # 創建 HumanMessage 對象
-        message = HumanMessage(content=input_text)
-        # 調用模型並獲取回答
-        response = model.invoke([message])
-        # 打印回答
-        print(response.content)
+        input_messages = [HumanMessage(input_text)]
+        output = app.invoke({"messages": input_messages}, config)
+        output["messages"][-1].pretty_print()  # output contains all messages in state
 
     input_text = input('>>> ')
