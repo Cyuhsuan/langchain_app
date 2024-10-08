47c6fa24aa58856f68686679589c658a7bcf1236
yuHsuan <x89301089@gmail.com>
2024-10-08 11:17:39 +0800

    [modify] 訊息修剪

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from langchain_openai import ChatOpenAI
-from langchain_core.messages import HumanMessage
+from langchain_core.messages import HumanMessage, AIMessage
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import START, MessagesState, StateGraph
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
@@ -7,8 +7,33 @@ from typing import Sequence
 from typing_extensions import Annotated, TypedDict
 from langchain_core.messages import BaseMessage
 from langgraph.graph.message import add_messages
-
+from langchain_core.messages import SystemMessage, trim_messages
 model = ChatOpenAI(model="gpt-3.5-turbo")
+
+trimmer = trim_messages(
+    max_tokens=65,
+    strategy="last",
+    token_counter=model,
+    include_system=True,
+    allow_partial=False,
+    start_on="human",
+)
+
+messages = [
+    SystemMessage(content="you're a good assistant"),
+    HumanMessage(content="hi! I'm bob"),
+    AIMessage(content="hi!"),
+    HumanMessage(content="I like vanilla ice cream"),
+    AIMessage(content="nice"),
+    HumanMessage(content="whats 2 + 2"),
+    AIMessage(content="4"),
+    HumanMessage(content="thanks"),
+    AIMessage(content="no problem!"),
+    HumanMessage(content="having fun?"),
+    AIMessage(content="yes!"),
+]
+
+trimmer.invoke(messages)
 
 prompt = ChatPromptTemplate.from_messages(
     [
@@ -28,7 +53,10 @@ class State(TypedDict):
 # Define the function that calls the model
 def call_model(state: State):
     chain = prompt | model
-    response = chain.invoke(state)
+    trimmed_messages = trimmer.invoke(state["messages"])
+    response = chain.invoke(
+        {"messages": trimmed_messages, "language": state["language"]}
+    )
     return {"messages": [response]}
 
 # Define a new graph
@@ -42,17 +70,24 @@ workflow.add_node("model", call_model)
 memory = MemorySaver()
 app = workflow.compile(checkpointer=memory)
 
-config = {"configurable": {"thread_id": "abc123"}}
-language = "Chinese"
+config = {"configurable": {"thread_id": "abc678"}}
+language = "English"
 
 input_text = input('>>> ')
 while input_text.lower() != 'bye':
     if input_text:
         input_messages = [HumanMessage(input_text)]
-        output = app.invoke(
+        # output = app.invoke(
+        #     {"messages": input_messages, "language": language},
+        #     config,
+        # )
+        # output["messages"][-1].pretty_print()  # output contains all messages in state
+        for chunk, metadata in app.stream(
             {"messages": input_messages, "language": language},
             config,
-        )
-        output["messages"][-1].pretty_print()  # output contains all messages in state
+            stream_mode="messages",
+        ):
+            if isinstance(chunk, AIMessage):  # Filter to just model responses
+                print(chunk.content, end="|")
 
     input_text = input('>>> ')
