11359a5d3fa66197301372de7070f279c0b88dcc
yu <x89301089@gmail.com>
2024-10-11 15:20:44 +0800

    [modify] langgraph quick start 3 添加 記憶

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from typing import Annotated
-
+from typing_extensions import TypedDict
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_core.messages import BaseMessage
-from typing_extensions import TypedDict
-
 from langgraph.graph import StateGraph
 from langgraph.graph.message import add_messages
 from langgraph.prebuilt import ToolNode, tools_condition
-
 from langchain_openai import ChatOpenAI
+from langgraph.checkpoint.memory import MemorySaver
+
+# 設定狀態
 class State(TypedDict):
     # Messages have the type "list". The `add_messages` function
     messages: Annotated[list, add_messages]
@@ -16,18 +16,12 @@ class State(TypedDict):
 # 設定模型
 model = ChatOpenAI(model="gpt-3.5-turbo")
 
+# 設定記憶體儲存
+memory = MemorySaver()
+
 # 設定工具
 tool = TavilySearchResults(max_results=2)
-# tools = [{
-#     "type": "function",
-#     "function": {
-#         "name": tool.name,
-#         "description": tool.description,
-#         "parameters": tool.args_schema.schema()
-#     }
-# }]
 tools = [tool]
-# tool.invoke("What's a 'node' in LangGraph?")
 # model 使用工具
 model = model.bind_tools(tools)
 
@@ -37,36 +31,31 @@ def chatbot(state: State):
 # 告宣告一個 state 的狀態機
 graph_builder = StateGraph(State)
 # # 添加聊天機器人節點
-# graph_builder.add_node("chatbot", chatbot)
-# # 添加工具節點
-# tool_node = ToolNode(tools=[tool])
-# graph_builder.add_node("tools", tool_node)
-
-# 添加邊
-# graph_builder.add_edge("tools", "chatbot")
-# graph_builder.set_entry_point("chatbot")
 graph_builder.add_node("chatbot", chatbot)
 
+# # 添加工具節點
 tool_node = ToolNode(tools=[tool])
 graph_builder.add_node("tools", tool_node)
 
+# 添加邊
+# 當使用工具時，返回聊天機器人節點
 graph_builder.add_conditional_edges(
     "chatbot",
     tools_condition,
 )
-# Any time a tool is called, we return to the chatbot to decide the next step
+# 任何時候使用工具時，返回聊天機器人節點
 graph_builder.add_edge("tools", "chatbot")
+# 設定入口節點
 graph_builder.set_entry_point("chatbot")
-graph = graph_builder.compile()
+# 編譯圖
+graph = graph_builder.compile(checkpointer=memory)
 
-# 建立 狀態機的樹圖
-# 它將是一個節點和邊的樹圖 (start -> (chatbot (if)-> tools -> chatbot) -> end)
-# graph = graph_builder.compile()
+config = {"configurable": {"thread_id": "1"}}
 
 def stream_graph_updates(user_input: str):
-    for event in graph.stream({"messages": [("user", user_input)]}):
+    for event in graph.stream({"messages": [("user", user_input)]}, config=config):
         for value in event.values():
-            print("Assistant:", value["messages"][-1].content)
+            value["messages"][-1].pretty_print()
 
 while True:
     try:
