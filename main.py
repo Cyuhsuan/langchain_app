a77578b4386ee0a22f4cbffce47d381d0968dc53
yu <x89301089@gmail.com>
2024-10-11 10:38:16 +0800

    [modify] langgraph quick start 1

--- main.py
+++ main.py
@@ -1,30 +1,49 @@
+from typing import Annotated
+from typing_extensions import TypedDict
 from langchain_openai import ChatOpenAI
-from langchain_core.messages import HumanMessage
-from langgraph.checkpoint.memory import MemorySaver
-from langgraph.graph import START, MessagesState, StateGraph
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
 model = ChatOpenAI(model="gpt-3.5-turbo")
-# Define a new graph
-workflow = StateGraph(state_schema=MessagesState)
-# Define the function that calls the model
-def call_model(state: MessagesState):
-    response = model.invoke(state["messages"])
-    return {"messages": response}
+class State(TypedDict):
+    # Messages have the type "list". The `add_messages` function
+    messages: Annotated[list, add_messages]
 
-# Define the (single) node in the graph
-workflow.add_edge(START, "model")
-workflow.add_node("model", call_model)
+def chatbot(state: State):
+    return {"messages": [model.invoke(state["messages"])]}
 
-# Add memory
-memory = MemorySaver()
-app = workflow.compile(checkpointer=memory)
 
-config = {"configurable": {"thread_id": "abc123"}}
+# 告宣告一個 state 的狀態機
+graph_builder = StateGraph(State)
+# 添加節點
+graph_builder.add_node("chatbot", chatbot)
+# 添加邊
+# START 是狀態機的入口
+# END 是狀態機的出口
+# 從 START 到 chatbot
+graph_builder.add_edge(START, "chatbot")
+# 從 chatbot 到 END
+graph_builder.add_edge("chatbot", END)
 
-input_text = input('>>> ')
-while input_text.lower() != 'bye':
-    if input_text:
-        input_messages = [HumanMessage(input_text)]
-        output = app.invoke({"messages": input_messages}, config)
-        output["messages"][-1].pretty_print()  # output contains all messages in state
+# 建立 狀態機的樹圖
+# it will be a tree of nodes and edges (start -> chatbot -> end)
+graph = graph_builder.compile()
 
-    input_text = input('>>> ')
+def stream_graph_updates(user_input: str):
+    for event in graph.stream({"messages": [("user", user_input)]}):
+        for value in event.values():
+            print("Assistant:", value["messages"][-1].content)
+
+while True:
+    try:
+        user_input = input(">>> ")
+        if user_input.lower() in ["quit", "exit", "q"]:
+            print("Goodbye!")
+            break
+
+        stream_graph_updates(user_input)
+    except:
+        # fallback if input() is not available
+        user_input = "What do you know about LangGraph?"
+        print("User: " + user_input)
+        stream_graph_updates(user_input)
+        break
